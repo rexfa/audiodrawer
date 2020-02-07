1bdd6b7cc69c026c0d84b841a2136ba523085f4e
rexfa <rex_fa@hotmail.com>
2020-02-07 19:27:15 +0800

    皮子先完成这个样子

---
+++ assets/nav.js
@@ -0,0 +1,6 @@
+showMainContent();
+
+function showMainContent() {
+    document.querySelector('.js-nav').classList.add('is-shown');
+    document.querySelector('.js-content').classList.add('is-shown');
+}
